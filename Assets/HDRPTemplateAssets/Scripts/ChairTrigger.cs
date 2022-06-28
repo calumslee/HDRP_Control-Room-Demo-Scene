9db6e922954f05531ef3c3399328f063080f18ff
Calum Slee <calumslee@hotmail.co.nz>
2022-06-29 08:56:03 +1200

    Project recovered

---
+++ Assets/HDRPTemplateAssets/Scripts/ChairTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _chair;
+
+    private bool _canTrigger;
+
+    private Vector3 _playerPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Trigger UI
+            Debug.Log("Press E");
+            _canTrigger = true;
+            _playerPos = other.transform.position;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Rotate chair towards Player
+            Debug.Log("Rotating Chair");
+        }
+    }
+}
---
+++ Assets/HDRPTemplateAssets/Scripts/DoorTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _door;
+    [SerializeField]
+    private GameObject _doorLight;
+
+    private Animator _doorAnim;
+
+    private bool _canOpen;
+
+    private void Start()
+    {
+        if (_door != null)
+        {
+            _doorAnim = _door.GetComponent<Animator>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Display UI
+            Debug.Log("Press E");
+
+            _canOpen = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_canOpen == true && Input.GetKeyDown(KeyCode.E))
+        {
+            _doorAnim.SetTrigger("openDoor");
+            //Change Light Emission
+        }
+    }
+}
